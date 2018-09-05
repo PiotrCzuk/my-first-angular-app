1edb5d70d46fd96fb4730e4a8ee2761287b2f41b
PiotrCzuk <p.michalc@gmail.com>
2018-09-05 14:05:33 +0200

    Recipe List added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe} from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'Some random recipe',
+      'http://services.supacache.com/php/IMAGES/FOODNETWORK_Polsat_Recipes/119---Image.jpg?t=1525074275')
+  ];
 
   constructor() { }
 
