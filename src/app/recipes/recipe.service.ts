db9e8b3db825ed4cc7d7f58d7fe03e5c3c9b5d00
PiotrCzuk <p.michalc@gmail.com>
2018-09-07 17:47:52 +0200

    ingredients passing across the app

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,37 @@
 import {Recipe} from './recipe.model';
-import {EventEmitter} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'Some random recipe',
-      'http://services.supacache.com/php/IMAGES/FOODNETWORK_Polsat_Recipes/119---Image.jpg?t=1525074275'),
-    new Recipe('Another test recipe', 'Some random recipe',
-      'http://services.supacache.com/php/IMAGES/FOODNETWORK_Polsat_Recipes/119---Image.jpg?t=1525074275')
+    new Recipe(
+      'A test recipe',
+      'Some random recipe',
+      'http://services.supacache.com/php/IMAGES/FOODNETWORK_Polsat_Recipes/119---Image.jpg?t=1525074275',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 15)
+      ]),
+    new Recipe(
+      'Another test recipe',
+      'Some random recipe',
+      'http://services.supacache.com/php/IMAGES/FOODNETWORK_Polsat_Recipes/119---Image.jpg?t=1525074275',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 3)
+      ])
 
   ];
 
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
