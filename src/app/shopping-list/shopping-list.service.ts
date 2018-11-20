fd38bdd7e1ebeebeadd9936bec507f53e193ecd4
PiotrCzuk <p.michalc@gmail.com>
2018-11-20 19:10:26 +0100

    Basic Validation + Editing Ingredients

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,10 @@
 import {Ingredient} from '../shared/ingredient.model';
 import {EventEmitter} from '@angular/core';
+import {Subject} from 'rxjs';
 
 export class ShoppingListService {
   ingredientsChanged = new EventEmitter<Ingredient[]>();
+  startedEditing = new Subject<number>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 4),
@@ -10,6 +12,9 @@ export class ShoppingListService {
 
   getIngredients() {
     return this.ingredients.slice();
+  }
+  getIngredient(index: number) {
+    return this.ingredients[index];
   }
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
