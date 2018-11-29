57fb174beb11ec09f34b971af18a9dae41513624
PiotrCzuk <p.michalc@gmail.com>
2018-11-29 19:11:41 +0100

    Modifying requests using interceptors

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams, HttpRequest} from '@angular/common/http';
 import 'rxjs/Rx';
 
 import {RecipeService} from '../recipes/recipe.service';
@@ -12,17 +12,28 @@ export class DataStorageService {
   constructor( private httpClient: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
 
   storeRecipes() {
-    const token = this.authService.getToken();
-    return this.httpClient.put('https://ng-recipe-book-9aef0.firebaseio.com/recipes.json?auth=' + token,
-      this.recipeService.getRecipes());
+    // const headers =  new HttpHeaders().set('Authorization', 'Bearer sdfjdnfgfg');
+
+    // return this.httpClient.put('https://ng-recipe-book-9aef0.firebaseio.com/recipes.json',
+    //   this.recipeService.getRecipes(), {
+    //   observe: 'body',
+    //     params: new HttpParams().set('auth', token)
+    //     // headers: headers
+    //   });
+    const req = new HttpRequest('PUT', 'https://ng-recipe-book-9aef0.firebaseio.com/recipes.json?',
+      this.recipeService.getRecipes(), {reportProgress: true});
+   return this.httpClient.request(req);
   }
   fetchRecipes() {
-    const token = this.authService.getToken();
-
-    return this.httpClient.get<Recipe[]>('https://ng-recipe-book-9aef0.firebaseio.com/recipes.json?auth=' + token)
+    // return this.httpClient.get<Recipe[]>('https://ng-recipe-book-9aef0.firebaseio.com/recipes.json?auth=' + token)
+    this.httpClient.get<Recipe[]>('https://ng-recipe-book-9aef0.firebaseio.com/recipes.json', {
+      observe: 'body',
+      responseType: 'json'
+    })
       .map(
         (recipes) => {
-          for (let recipe of recipes) {
+          console.log(recipes);
+          for (const recipe of recipes) {
             if ( !recipe['ingredients']) {
               recipe['ingredients'] = [];
             }
